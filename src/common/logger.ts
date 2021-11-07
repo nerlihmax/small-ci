3297b92fb33b18116adb30be565d24aa5eb61fcb
Nerlih Max <nerlihmax@yandex.ru>
2021-11-07 19:14:37 +0700

    feat: дополнительные логи

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -2,5 +2,5 @@ export let DEBUG = false;
 
 export const setDebug = (state: boolean) => DEBUG = state;
 
-export const DERROR = console.error;
-export const DLOG = console.log;
+export const DERROR = (msg: string) => console.error(`ERROR: ${msg}`);
+export const DLOG = (msg: string) => console.log(`LOG: ${msg}`);
