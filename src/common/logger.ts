aadfa520f2b406975c1f32c27f2bec202d06bb73
Nerlih Max <nerlihmax@yandex.ru>
2021-11-08 06:27:41 +0700

    fix: теперь D** работает только при DEBUG===true

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -2,5 +2,5 @@ export let DEBUG = false;
 
 export const setDebug = (state: boolean) => DEBUG = state;
 
-export const DERROR = console.error;
-export const DLOG = console.dir;
+export const DERROR = (msg: any) => DEBUG && console.error(msg);
+export const DLOG = (msg: any) => DEBUG && console.dir(msg);
